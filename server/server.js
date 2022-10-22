aa6542867e3014e5b5f34f9ddaed4c9b4b09164f
President <aaron.m.wilson@uconn.edu>
2022-10-22 04:58:16 -0400

    might work better if i actually installed the apollo package

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
-const express = require('express');
+
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 

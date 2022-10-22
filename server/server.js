eec07c0467e202eb7a2cbb98d1a090e67ec23e8f
President <aaron.m.wilson@uconn.edu>
2022-10-22 04:56:56 -0400

    importing apollo

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
